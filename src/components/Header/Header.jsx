cd6fe0c7db81052ebdd1446e7f4fe205170ec4f1
Kirill Lavrov <lkv@myttk.ru>
2022-08-10 23:16:31 +0700

    continum work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import {urlAuth} from '../../api/auth';
 import {useAuth} from '../../hooks/useAuth';
 import {Layout} from '../Layout/Layout';
 import style from './Header.module.css';
+import {ReactComponent as NoAuth} from './img/no-auth.svg';
 
 export const Header = () => {
   const {logout, isAuth, auth} = useAuth();
@@ -9,19 +10,29 @@ export const Header = () => {
   console.log(style);
 
   return (
-    <header>
-      <Layout>
-        <button onClick={logout}>Очистить токен</button>
-        <a href={urlAuth}>Авторизация</a>
-        {isAuth && (
-          <>
-            <button className={style.button}>
-              <span>{auth.first_name}</span>
-              <img src={auth.profile_image.medium} alt="" />
-            </button>
-          </>
-        )}
-      </Layout>
+    <header className={style.header}>
+      <div className={style['header-front']}>
+        <Layout>
+          <div className={style.content}>
+            <NoAuth className={style.logo} />
+            <div className={style.auth}>
+              <a className={style['auth-link']} href={urlAuth}>
+                <NoAuth />
+              </a>
+              <button onClick={logout}>Logout</button>
+              {isAuth && (
+                <>
+                  <button className={style.button}>
+                    <span>{auth.first_name}</span>
+                    <img src={auth.profile_image.medium} alt="" />
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </Layout>
+      </div>
+      <div className={style['header-back']}></div>
     </header>
   );
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,46 @@
+import { Layout } from '../Layout/Layout';
 import style from './Main.module.css';
 
 export const Main = () => {
   console.log(style);
   return (
-    <div></div>
+    <main className={style.main}>
+      <Layout>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, libero facilis eum nisi architecto vero dolorum! 
+          Maiores aut explicabo vitae delectus ut ducimus repellendus nam sed minus nemo eius nesciunt consequuntur, obcaecati 
+          doloremque magni aliquid saepe placeat. Ab inventore veniam perferendis, excepturi laborum commodi explicabo sunt 
+          accusantium ut id eius officia, quibusdam, expedita dignissimos repellendus consequatur quas illum aliquid veritatis 
+          assumenda doloremque quidem quisquam nobis. Ipsum, nam tempora? Temporibus placeat ullam, deleniti necessitatibus,
+          exercitationem rem odit expedita modi ex eum totam quam distinctio ipsum nemo provident ratione facilis cumque corporis
+          eos fugit quis! Ab sequi incidunt eligendi odio. Ipsam nostrum non voluptatibus reiciendis. Ipsa aperiam id temporibus
+          deserunt odio consequatur culpa reprehenderit similique aliquam atque quis dolore dolorum iusto officia, obcaecati 
+          sequi officiis distinctio, nulla minus adipisci aut harum? Ipsa similique eveniet fuga provident culpa dolorem 
+          distinctio quaerat rem est placeat impedit ipsam eos reiciendis molestias pariatur laudantium eum perspiciatis illum,
+          enim maxime laboriosam tempora illo dolores. Vel quod quos cupiditate tempora pariatur laborum. Laudantium aspernatur
+          quis soluta blanditiis et labore quia cupiditate nam adipisci accusantium quo ducimus inventore recusandae nemo
+          deserunt maxime culpa quas facere, molestias quisquam, eveniet asperiores dolores aliquid similique. Delectus 
+          accusantium consequatur natus aperiam nostrum! Beatae quidem perferendis nisi cum quos est repudiandae commodi 
+          repellendus fugit iure quasi consequuntur culpa amet minima corrupti doloribus suscipit iusto, aut voluptatem tenetur 
+          animi unde harum tempore! Adipisci eveniet numquam temporibus distinctio velit mollitia soluta itaque facilis sed 
+          accusamus, est voluptatum, modi non cum reiciendis, dolorum voluptas incidunt similique ratione aliquid. Ad beatae 
+          eius est reiciendis in ipsum quisquam nemo laudantium eligendi voluptate provident, tenetur, laboriosam corrupti! 
+          Saepe reiciendis iusto natus debitis pariatur quod similique. Porro inventore sequi provident veniam, nostrum sint ad 
+          illum. Sed, aut! Error quasi repudiandae deserunt neque odit quibusdam in consequatur eligendi quo, facere expedita, 
+          omnis, dicta autem suscipit totam cupiditate reprehenderit a libero vero reiciendis voluptates. Aliquam ipsam fugit 
+          minus? Eius sunt autem pariatur harum reprehenderit ut vitae quo itaque ratione consequuntur ea earum quae laboriosam 
+          ducimus laborum dignissimos ipsa, tenetur ipsam quisquam sed rem laudantium minima iure cum! Exercitationem repellendus 
+          fugiat in ea culpa nobis magni laboriosam nulla ad voluptatibus nam, repudiandae dignissimos excepturi illo perferendis 
+          obcaecati ducimus. Odit praesentium facere libero voluptatibus? Delectus fugiat ullam nobis doloremque omnis beatae 
+          quam nesciunt, dolores rerum incidunt sit ad dolorem consequatur id architecto sed error quod consequuntur harum 
+          eligendi debitis similique. Animi voluptate illum cum impedit tempore praesentium dolorem labore dicta amet? Eligendi 
+          quisquam dolorem molestias nisi rerum iusto, ut fuga at autem ad. Id veritatis ad dicta culpa ullam officiis suscipit 
+          pariatur dignissimos alias cupiditate facilis aspernatur officia ipsam velit in molestias corporis, voluptates voluptatem 
+          laboriosam laudantium. Ipsa expedita natus labore, libero quisquam quae nam distinctio tempore adipisci necessitatibus 
+          debitis dicta! Debitis necessitatibus vitae quam perspiciatis eligendi at obcaecati eaque deserunt, ipsam nisi tempora 
+          sit rem quasi esse quod doloribus. Voluptatibus et dignissimos aspernatur? Maiores aut sint optio quibusdam inventore 
+          iste. Velit, molestias amet unde rerum asperiores, nostrum ut quaerat reprehenderit et, tenetur modi recusandae.
+          </p>
+      </Layout>
+    </main>
   );
 };
